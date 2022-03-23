237ece60140d9c6996ca8a2d4b59f2ebc4ccfa78
Ibnu Naufal <ibnunaufal5758@gmail.com>
2022-03-23 20:44:19 +0700

    fix

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 @section('content')
+<!-- 
+https://drive.google.com/file/d//view?usp=sharing
+https://drive.google.com/file/d/1sHs4WXIkBtQvfva3sw106-JppH_DhVnJ/view?usp=sharing
+https://drive.google.com/file/d/1sHs4WXIkBtQvfva3sw106-JppH_DhVnJ/view?usp=sharing
+https://drive.google.com/file/u/5/d/19kT1qe4amR-OM4DI65nWgRHNtI5A-6S3/view?usp=drive_open -->
 <main>
     <div style="
     margin-left: 10%;
@@ -22,6 +27,7 @@
             <a class="btn btn-primary" href=" {{ url('category/create') }}">Add New</a>
         </div>
         
+        <div style="overflow-x:auto;">
         <table class="table-bordered" style="width: 100%;">
             <tr>
                 <th class="td-bordered">No</th>
@@ -37,8 +43,12 @@
                 <tr>
                     <td class="td-bordered">{{ ++$i }}</td>
                     <td class="td-bordered">{{ $category->name }}</td>
-                    <td class="td-bordered"><h6> {{ strpos( $category->image, "/" ) </h6></td>
-                    <td class="td-bordered">{{ $category->pdf }}</td>
+                    <td class="td-bordered">
+                        <a href="{{ $category->image }}" target="_blank">Gambar</a>
+                    </td>
+                    <td class="td-bordered">
+                        <a href="{{ $category->pdf }}" target="_blank">PDF</a>
+                    </td>
                     <td class="td-bordered" style="text-align: center;">
                         <form action="{{ route('category.destroy',$category->id) }}" method="POST" onsubmit="return myFunction()">
                             <a class="btn btn-info" href="{{ route('category.show',$category->id) }}">Show</a>
@@ -51,6 +61,7 @@
                 </tr>
             @endforeach
         </table>
+        </div>
     </div>
 
     <br>
